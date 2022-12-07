30355b759b5448c15eb647e2bf356039efa8dded
Praveen Kumar Singh <117644194+Praveen9821@users.noreply.github.com>
2022-12-07 22:47:08 +0530

    Add files via upload

---
+++ My first python class.py
@@ -0,0 +1,133 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=10
+a
+10
+b=20
+b
+20
+a=10
+b=30
+c=a+b
+print(c)
+40
+d=40
+e=50
+f=d+e
+print(f)
+90
+g=60
+h=40
+j=g-h
+print(j)
+20
+v=50
+x=50
+z=v*x
+print(z)
+2500
+m=40
+n=20
+l=m/n
+prrint(l)
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    prrint(l)
+NameError: name 'prrint' is not defined. Did you mean: 'print'?
+L=m/n
+print(L)
+2.0
+a=10
+b=20
+c=a
+a=b
+b=c
+a
+20
+b
+10
+c
+10
+print('Hello, World')
+Hello, World
+#This is a comment.
+x=5
+y=praveen
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    y=praveen
+NameError: name 'praveen' is not defined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x=5
+y='praveen'
+print(x)
+5
+print(y)
+praveen
+z=x+y
+Traceback (most recent call last):
+  File "<pyshell#56>", line 1, in <module>
+    z=x+y
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+print('x+y')
+x+y
+x=5
+y=raj
+Traceback (most recent call last):
+  File "<pyshell#59>", line 1, in <module>
+    y=raj
+NameError: name 'raj' is not defined
+x=5
+y='raj'
+print('x+y')
+x+y
+z='x+y'
+a='praveen'*'9'
+Traceback (most recent call last):
+  File "<pyshell#64>", line 1, in <module>
+    a='praveen'*'9'
+TypeError: can't multiply sequence by non-int of type 'str'
+print('praveen'*'9')
+Traceback (most recent call last):
+  File "<pyshell#65>", line 1, in <module>
+    print('praveen'*'9')
+TypeError: can't multiply sequence by non-int of type 'str'
+a="abc"
+print('a' + '6')
+a6
+a="abc"
+print(" "*100,"abc")
+                                                                                                     abc
+print("abc"*100,"def")
+abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc def
+print("praveen"*9,)
+praveenpraveenpraveenpraveenpraveenpraveenpraveenpraveenpraveen
+print("abc"*50,)
+abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc
+2+4j
+(2+4j)
